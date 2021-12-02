047e75561edb51cee919c77e3be8db31c23aa86a
Brad Breiten <jonathan@breiten.us>
2021-12-02 11:57:46 -0700

    Update route for galleries/:id

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/directors', to: 'directors#index'
   get '/directors/:id', to: 'directors#show'
-  
+
   get '/galleries', to: 'galleries#index'
+  get 'galleries/:id', to: 'galleries#show'
 end
