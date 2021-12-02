d25cf0551eaac57879b1830eff61ab8439b7e825
Tim Roeder <timothyjroeder@gmail.com>
2021-12-01 23:41:21 -0700

    Fix: changed let! symbols to generic names

--- spec/features/directors/index_spec.rb
+++ spec/features/directors/index_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe '/director/index.html.erb', type: :feature do
-  let!(:wes) { Director.create!(name: 'Wes Anderson', imdb_rank: 20, tv_credit: false) }
-  let!(:steven) { Director.create!(name: 'Steven Spielberg', imdb_rank: 1, tv_credit: true) }
+  let!(:director_1) { Director.create!(name: 'Wes Anderson', imdb_rank: 20, tv_credit: false) }
+  let!(:director_2) { Director.create!(name: 'Steven Spielberg', imdb_rank: 1, tv_credit: true) }
 
   describe 'as a user' do
     describe 'when I visit the directors index' do
       it 'displays all the directors names' do
         visit '/directors'
 
-        expect(page).to have_content(wes.name)
-        expect(page).to have_content(steven.name)
+        expect(page).to have_content(director_1.name)
+        expect(page).to have_content(director_2.name)
       end
     end
   end
