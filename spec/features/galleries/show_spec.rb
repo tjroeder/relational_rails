97e9e3c7d07fd2350cacec1b2854e39b5f944bf4
Brad Breiten <jonathan@breiten.us>
2021-12-02 13:23:02 -0700

    Add user story two tests for parent table attribute display

--- spec/features/galleries/show_spec.rb
+++ spec/features/galleries/show_spec.rb
@@ -6,12 +6,18 @@ RSpec.describe GalleriesController, type: :feature do
       @gallery_1 = Gallery.create(name: "Art 4 Us", non_profit: true, entry_cost: 4.75)
       @gallery_2 = Gallery.create(name: "Benji's Gallery", non_profit: false, entry_cost: 10)
 
-      visit 'galleries/:id'
-    end
-    it 'should have a page at parents/:id' do
-      expect(page).to have_current_path('/galleries/:id')
+      visit "galleries/"
+      click_link "Art 4 Us"
     end
 
+    it 'should visit the page at parents/:id' do
+      expect(page).to have_current_path("/galleries/#{@gallery_1.id}")
+    end
 
+    it 'displays gallery attributes on page' do
+      expect(page).to have_content(@gallery_1.name)
+      expect(page).to have_content('NPO Status: "Non-Profit"')
+      expect(page).to have_content("Cost of Entry: $#{@gallery_1.entry_cost}")
+    end
   end
 end
