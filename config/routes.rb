5f116e9fe746f2447d4672cba1ed0a7793f59876
Tim Roeder <timothyjroeder@gmail.com>
2021-12-01 23:39:15 -0700

    Feat: created directors#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/directors', to: 'directors#index'
+  get '/directors/:id', to: 'directors#show'
   
   get '/galleries', to: 'galleries#index'
 end
