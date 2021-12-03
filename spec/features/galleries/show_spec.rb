adcc1ba9ef774992e47866fefe9682e154c8a5ab
Brad Breiten <jonathan@breiten.us>
2021-12-03 15:31:16 -0700

    Add test for total pieces on gallery show page

--- spec/features/galleries/show_spec.rb
+++ spec/features/galleries/show_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe GalleriesController, type: :feature do
       @gallery_1 = Gallery.create!(name: "Art 4 Us", non_profit: true, entry_cost: 4.75)
       @gallery_2 = Gallery.create!(name: "Benji's Gallery", non_profit: false, entry_cost: 10)
 
+      @piece_1 = @gallery_1.pieces.create!(name: "Starry Night", artist: "Vincent Van Gogh", year: 1889, original: false)
+      @piece_2 = @gallery_1.pieces.create!(name: "Self Portrait", artist: "Vincent Van Gogh", year: 1889, original: true)
+      @piece_3 = @gallery_2.pieces.create!(name: "Girl with the Pearl Earring", artist: "Johannes Vermeer", year: 1665, original: false)
+
       visit "galleries/"
       click_link "Art 4 Us"
     end
@@ -18,6 +22,10 @@ RSpec.describe GalleriesController, type: :feature do
       expect(page).to have_content(@gallery_1.name)
       expect(page).to have_content('NPO Status: "Non-Profit"')
       expect(page).to have_content("Cost of Entry: $#{@gallery_1.entry_cost}")
+    end
+
+    it 'shows count of number of pieces associated with gallery' do
+      expect(page).to have_content("Number of Pieces: #{@gallery_1.total_pieces}")
     end
   end
 end
