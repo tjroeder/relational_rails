ccbc33296f2307bc6aa196280cd43a8f5f51cbf5
Brad Breiten <jonathan@breiten.us>
2021-12-03 10:13:29 -0700

    Add gallery_pieces controller route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/directors', to: 'directors#index'
   get '/directors/:id', to: 'directors#show'
-  
+
   get '/films', to: 'films#index'
   get '/films/:id', to: 'films#show'
 
   get '/galleries', to: 'galleries#index'
-  get 'galleries/:id', to: 'galleries#show'
+  get '/galleries/:id', to: 'galleries#show'
+  get '/galleries/:artist_id/pieces', to: 'gallery_pieces#index'
 
   get '/pieces', to: 'pieces#index'
   get '/pieces/:id', to: 'pieces#show'
