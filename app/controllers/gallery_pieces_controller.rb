4b3b75ca012f57eb9fcfeb4b3edc5391548fb846
Brad Breiten <jonathan@breiten.us>
2021-12-04 10:48:17 -0700

    Feat: add #new and #create methods to gallery_pieces_controller

--- app/controllers/gallery_pieces_controller.rb
+++ app/controllers/gallery_pieces_controller.rb
@@ -5,4 +5,23 @@ class GalleryPiecesController < ApplicationController
     @pieces = @gallery.pieces
   end
 
+  def new
+    @gallery = Gallery.find(params[:gallery_id])
+  end
+
+  def create
+    gallery = Gallery.find(params[:piece][:gallery_id])
+
+    piece = gallery.pieces.new({
+      name: params[:piece][:name],
+      artist: params[:piece][:artist],
+      year: params[:piece][:year],
+      original: params[:piece][:original]
+      })
+
+    gallery.save
+
+    redirect_to "/galleries/#{gallery.id}/pieces"
+  end
+
 end
