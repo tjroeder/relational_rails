51cd26bf48a3b0fc5f7af86fb584a97c3024388a
Tim Roeder <timothyjroeder@gmail.com>
2021-12-04 21:12:16 -0700

    Feat: add GET and POST routes for creating a new director film

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/directors/:id', to: 'directors#show'
   get '/directors/:id/edit', to: 'directors#edit'
   get '/directors/:director_id/films', to: 'director_films#index'
+  get '/directors/:director_id/films/new', to: 'director_films#new'
   post '/directors', to: 'directors#create'
+  post '/directors/:director_id/films', to: 'director_films#create'
   patch '/directors/:id', to: 'directors#update'
 
   get '/films', to: 'films#index'
