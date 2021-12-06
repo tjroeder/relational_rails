319783debb8b35d2441a3c634f36690c9c8a8a89
Brad Breiten <jonathan@breiten.us>
2021-12-06 15:49:39 -0700

    Test: add tests to cover added edit link in gallery_pieces index page

--- spec/features/galleries/pieces/index_spec.rb
+++ spec/features/galleries/pieces/index_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Gallery pieces index' do
     @piece_1 = @gallery_1.pieces.create!(name: "Starry Night", artist: "Vincent Van Gogh", year: 1889, original: false)
     @piece_2 = @gallery_1.pieces.create!(name: "Self Portrait", artist: "Vincent Van Gogh", year: 1889, original: true)
     @piece_3 = @gallery_2.pieces.create!(name: "Girl with the Pearl Earring", artist: "Johannes Vermeer", year: 1665, original: false)
+    @piece_4 = @gallery_1.pieces.create!(name: "Mona Lisa", artist: "Leonardo DaVinci", year: 1665, original: true, gallery_id: @gallery_2.id)
 
     visit "/galleries/#{@gallery_1.id}/pieces"
   end
@@ -74,6 +75,25 @@ RSpec.describe 'Gallery pieces index' do
       expect(piece_5.name).to appear_before(@piece_2.name, only_text: true)
       expect(@piece_2.name).to appear_before(@piece_1.name, only_text: true)
       expect(@piece_1.name).to appear_before(piece_4.name, only_text: true)
+    end
+  end
+
+  describe 'edit piece info link' do
+    it 'has a link that navigates to piece edit info page' do
+      expect(page).to have_link("Edit", :href=>"/pieces/#{@piece_4.id}/edit")
+      expect(page).to have_link("Edit", :href=>"/pieces/#{@piece_2.id}/edit")
+    end
+
+    it 'naviagtes to the edit page' do
+      page.find(:css, "##{@piece_4.id}").click_on
+
+      expect(current_path).to eq("/pieces/#{@piece_4.id}/edit")
+
+      visit "/pieces"
+
+      page.find(:css, "##{@piece_2.id}").click_on
+
+      expect(current_path).to eq("/pieces/#{@piece_2.id}/edit")
     end
   end
 end
