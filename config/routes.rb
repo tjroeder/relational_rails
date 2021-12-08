a7687b6098145d803c37aba82f4cf0ad0cb14a2f
Tim Roeder <timothyjroeder@gmail.com>
2021-12-07 21:39:20 -0700

    Fix: removed boilerplate comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/directors', to: 'directors#index'
   get '/directors/new', to: 'directors#new'
   get '/directors/:id', to: 'directors#show'
