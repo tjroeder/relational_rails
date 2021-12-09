38c54fd2c00556c2fe94177d5b322ccf6f0f885d
Brad Breiten <jonathan@breiten.us>
2021-12-09 11:10:51 -0700

    Add model tests for gallery class methods refactor

--- spec/models/gallery_spec.rb
+++ spec/models/gallery_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Gallery, type: :model do
-  let(:gallery_1) {Gallery.create!(name: "Art 4 Us", non_profit: true, entry_cost: 4.75)}
+  let(:gallery_1) {Gallery.create!(name: "Art 4 Us", non_profit: true, entry_cost: 4.75, created_at: '2021-12-03 20:42:56 UTC')}
+
+  let(:gallery_2) {Gallery.create!(name: "Benji's Gallery", non_profit: false, entry_cost: 10, created_at: '2021-12-03 20:43:52 UTC')}
 
   describe 'relationships' do
     it { should have_many :pieces }
@@ -19,6 +21,20 @@ RSpec.describe Gallery, type: :model do
       piece_3 = gallery_1.pieces.create!(name: "Girl with the Pearl Earring", artist: "Johannes Vermeer", year: 1665, original: false)
 
       expect(gallery_1.total_pieces).to eq(3)
+    end
+  end
+
+  describe 'class methods' do
+    it 'orders galleries in descending order of create date' do
+      expect(Gallery.created_at_order_desc).to eq([gallery_2, gallery_1])
+    end
+
+    it 'orders galleries based on number of pieces they house' do
+      piece_1 = gallery_1.pieces.create!(name: "Starry Night", artist: "Vincent Van Gogh", year: 1889, original: false)
+      piece_2 = gallery_2.pieces.create!(name: "Self Portrait", artist: "Vincent Van Gogh", year: 1889, original: true)
+      piece_3 = gallery_2.pieces.create!(name: "Girl with the Pearl Earring", artist: "Johannes Vermeer", year: 1665, original: false)
+
+      expect(Gallery.order_by_piece_count).to eq([gallery_2, gallery_1])
     end
   end
 end
